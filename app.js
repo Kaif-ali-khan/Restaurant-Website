5533a5aea1328448890420e5df3a5b7ba676cb9d
Kaif-ali-khan <kaifalikhan603@gmail.com>
2024-07-12 18:57:09 +0500

    section5 is done except for the slider

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ text.innerHTML = text.innerText
     (char, i) => `<span style="transform:rotate(${i * 25}deg)">${char}</span>`
   )
   .join("");
+
+
+  // testimonial slider
+
+  // $(document).ready(function(){
+  //   $('.slider-boxes').owlCarousel({
+  //     loop:true,
+  //     margin:10,
+  //     nav:false,
+  //     dots:true,
+  //     responsive:{
+  //         0:{
+  //             items:1
+  //         },
+  //         600:{
+  //             items:1
+  //         },
+  //         1000:{
+  //             items:1
+  //         }
+  //     }
+  // })  
+  // });
